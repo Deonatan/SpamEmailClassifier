65841b8cf6aa149f6f70fd1ade03a44b76e8c7b2
Deonatan Dinata <69316014+Deonatan@users.noreply.github.com>
2021-02-08 21:00:45 +0800

    get the list of vocab from vocab.txt

---
+++ getVocabList.m
@@ -0,0 +1,10 @@
+function vocabList = getVocabList()
+  fid = fopen('vocab.txt'); 
+  n = 1899 %total vocab number in vocab.txt
+  vocabList = cell(n,1); 
+  for i = 1:n
+    fscanf(fid, '%d' , 1); 
+    vocabList{i} = fscanf(fid, '%s' , 1);
+  endfor
+  fclose(fid); 
+endfunction
